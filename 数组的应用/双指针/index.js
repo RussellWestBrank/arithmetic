cef8d8ed04ba86e507ed9cd0987a35ea949e0d74
Russ <wangqingqiang@xhzy.onaliyun.com>
2024-02-23 13:55:21 +0800

    feat: 有序数组合并

--- 数组的应用/双指针/index.js
+++ 数组的应用/双指针/index.js
@@ -15,24 +15,30 @@ nums2 = [2,5,6], n = 3
  * @param {number} n
  * @return {void} Do not return anything; modify num1 in-place instead
  */
-let nums1 = [1,2,3,0,0,0], m = 3
-let nums2 = [2,5,6], n = 3
-function merge(num1, m, nums2, n) {
-    let i = m -1, j = n -1, k = m + n -1;
-    while(i >=0 && j >=0 ){
-        if(num1[i] < nums2[j]) {
-            num1[k] = nums2[j]
-            j--
+let nums1 = [1,2,7,0,0,0], m = 3
+let nums2 = [1,5,6], n = 3
+const merge = function(nums1, m, nums2, n) {
+    // 初始化两个指针的指向，初始化 nums1 尾部索引k
+    let i = m - 1, j = n - 1, k = m + n - 1
+    // 当两个数组都没遍历完时，指针同步移动
+    while(i >= 0 && j >= 0) {
+        // 取较大的值，从末尾往前填补
+        if(nums1[i] >= nums2[j]) {
+            nums1[k] = nums1[i] 
+            i-- 
             k--
-        }else{
-            num1[k] = num1[i]
-            i--
+        } else {
+            nums1[k] = nums2[j] 
+            j-- 
             k--
         }
     }
-
-    if(i < 0) {
-        num1 = [...nums2, ...num1]
+    
+    // nums2 留下的情况，特殊处理一下 
+    while(j>=0) {
+        nums1[k] = nums2[j]  
+        k-- 
+        j--
     }
 }
 merge(nums1, m, nums2, n)
